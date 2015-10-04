ed96910354caa13dadb22585d415b725bf0e2752
jiaz <jiaji.zh@gmail.com>
2015-10-04 02:22:55 -0700

    avoid error output when cask is not installed

--- osxtools/setup.sh
+++ osxtools/setup.sh
@@ -3,5 +3,5 @@
 brew install readline gdbm icu4c sqlite
 brew install the_silver_searcher ctags
 
-brew list brew-cask | grep 'brew-cask' > /dev/null || brew install caskroom/cask/brew-cask
+brew list brew-cask 2>&1 | grep 'brew-cask' > /dev/null || brew install caskroom/cask/brew-cask
 brew update && brew upgrade && brew cleanup && brew cask cleanup
