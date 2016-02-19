0914bbdb1454cd574824659692ad15ae29505b2e
Jiaji Zhou <jiaji.zhou@hulu.com>
2016-02-19 15:23:06 -0800

    add zprezto

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -10,8 +10,13 @@ sudo_append_if_missing '/usr/local/bin/zsh' /etc/shells
 chsh -s /usr/local/bin/zsh
 
 # Install oh my zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Link zshrc
-create_link ${1}/zshrc ~/.zshrc
-create_link ${1}/zlogin ~/.zlogin
+create_link ${1}/zprezto/ ~/.zprezto
+create_link ${1}/zprezto/runcoms/zshrc ~/.zshrc
+create_link ${1}/zprezto/runcoms/zshenv ~/.zshenv
+create_link ${1}/zprezto/runcoms/zlogin ~/.zlogin
+create_link ${1}/zprezto/runcoms/zlogout ~/.zlogout
+create_link ${1}/zprezto/runcoms/zpreztorc ~/.zpreztorc
+create_link ${1}/zprezto/runcoms/zprofile ~/.zprofile
